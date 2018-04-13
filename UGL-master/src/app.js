da0844c01cb0ecb14d22ad0810c512e8a83c911d
Colstad Joseph <MQI7685@hca.corpad.net>
2018-04-13 08:30:13 -0500

    This is Contacts and Orgs

--- UGL-master/src/app.js
+++ UGL-master/src/app.js
@@ -5,15 +5,19 @@ var App = angular.module('Urban',
         $locationProvider.hashPrefix('');
         $routeProvider
             .when('/', {
-                redirectTo: '/landing',
+                redirectTo: '/allOrganizations',
             })
             .when('/landing', {
-                redirectTo: '/newContact'
+                redirectTo: '/allContacts'
             })
             // .when('/landing', {
             //     templateUrl: '../src/templates/landing.html',
             //     controller: 'LandingController'
             // })
+            .when('/allOrganizations', {
+                templateUrl: '../src/templates/allOrganizations.html',
+                controller: 'OrganizationsController'
+            })
             .when('/editor', {
                 templateUrl: '../src/templates/editor.html',
                 controller: 'EditorController'
@@ -27,24 +31,24 @@ var App = angular.module('Urban',
                 templateUrl: '../src/templates/newContact.html',
                 controller: "NewContactController"
             })
-            .when('/contactInfo/:id',{
-                //view contact with edit option
-                templateUrl: '../src/templates/contactInfo.html',
-                controller: 'ContactInfoController'
-            })
-            .when('/newOrganization', {
-                templateUrl: '../src/templates/newOrganization.html',
-                controller: 'NewOrganizationController'
-            })
-            .when('/organizationInfo/:id',{
-                templateUrl: '../src/templates/organizationInfo.html',
-                controller: 'OrganizationInfoController'
-            })
-            /*
+            // .when('/contactInfo/:id',{
+            //     //view contact with edit option
+            //     templateUrl: '../src/templates/contactInfo.html',
+            //     controller: 'ContactInfoController'
+            // })
+            // .when('/newOrganization', {
+            //     templateUrl: '../src/templates/newOrganization.html',
+            //     controller: 'NewOrganizationController'
+            // })
+            // .when('/organizationInfo/:id',{
+            //     templateUrl: '../src/templates/organizationInfo.html',
+            //     controller: 'OrganizationInfoController'
+            // })
+            
             .when('/allContacts',{
                 templateUrl: '../src/templates/allContacts.html',
-                controller: 'AllContacts'
-            })
+                controller: 'ContactsController'
+            })/*
             .when('/allOrganizations',{
                 templateUrl: '../src/templates/allOrganizations.html',
                 controller: 'AllOrganizations'
@@ -53,20 +57,20 @@ var App = angular.module('Urban',
                 redirectTo: '/landing'
             });
     })
-    .directive('pdetails', function () {
+    .directive('info', function () {
         return {
-            restrict: 'EA',
-            scope: {
-                detailData: '=project',
-                //maybe use ng-switch based on mode edit/read/detailed/summary....???
-            },
-            transclude: true,
-            link: function (scope, element, attrs, ctrl, transclude) {
-                transclude(scope, function (clone, scope) {
-                    element.append(clone);
-                });
-            },
-            templateUrl: '../src/templates/project-details.html'
+            // restrict: 'EA',
+            // scope: {
+            //     thisContact: '=data',
+            //     //maybe use ng-switch based on mode edit/read/detailed/summary....???
+            // },
+            // transclude: true,
+            // link: function (scope, element, attrs, ctrl, transclude) {
+            //     transclude(scope, function (clone, scope) {
+            //         element.append(clone);
+            //     });
+            // },
+            templateUrl: '../src/templates/dispContact.html'
         }
     })
     .directive('project', function () {
